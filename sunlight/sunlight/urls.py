4a6b90492d6bbd07a13da425abc18d7351e44bb1
Liam Boog <mothercow5@gmail.com>
2024-04-04 19:46:22 -0400

    init sunlight app

--- sunlight/sunlight/urls.py
+++ sunlight/sunlight/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import sunlight_app.views
 
 urlpatterns = [
+    path("sunlight_app/", sunlight_app.views.index, name="sunlight_app"),
     path('admin/', admin.site.urls),
 ]
---
+++ sunlight/sunlight_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SunlightAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sunlight_app'
