c7790ff9b0689459cdbc3d50f34a405c1098a954
Liam Boog <mothercow5@gmail.com>
2024-04-04 20:17:27 -0400

    init db + ignore pycache

--- sunlight/sunlight_app/models.py
+++ sunlight/sunlight_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Program(models.Model):
+    name = models.CharField(max_length=256)
+    wake_time = models.TimeField()
+    
+    def __str__(self):
+        return self.name
