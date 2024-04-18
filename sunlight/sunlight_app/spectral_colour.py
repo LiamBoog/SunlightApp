f7ccc27d27d046146c8a21adf876b4a40b1141a5
Liam Boog <mothercow5@gmail.com>
2024-04-18 19:41:52 -0400

    actual good colour mapping

--- sunlight/sunlight_app/spectral_colour.py
+++ sunlight/sunlight_app/spectral_colour.py
@@ -1,17 +1,20 @@
 import colour
+import matplotlib.colors
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.constants import h, c, k
 
 
-# spectral_distribution = colour.sd_blackbody(4000)
+# spectral_distribution = colour.sd_blackbody(4000) * 1e9
 # with colour.domain_range_scale("1"):
 #     xyz = colour.sd_to_XYZ(spectral_distribution)
+#     xyz /= np.sum(xyz)
 #     xy = colour.XYZ_to_xy(xyz)
 #     xy = colour.CCT_to_xy(1000)
-#     print(xy)
 #     xyz = colour.xy_to_XYZ(xy)
 #     rgb = colour.XYZ_to_sRGB(xyz)
-#     colour.plotting.plot_single_colour_swatch(rgb)
+#     print(rgb)
+#     # colour.plotting.plot_single_colour_swatch(rgb)
 # xy = colour.CCT_to_xy(1500)
 # xyz = colour.xy_to_XYZ(xy)
 # rgb = colour.XYZ_to_sRGB(xyz)
@@ -58,7 +61,7 @@ class ColourSystem:
         # xyz -> rgb transformation matrix
         self.T = self.MI / self.wscale[:, np.newaxis]
 
-    def xyz_to_rgb(self, xyz, out_fmt=None):
+    def xyz_to_rgb(self, xyz):
         """Transform from xyz to rgb representation of colour.
 
         The output rgb components are normalized on their maximum
@@ -79,9 +82,8 @@ class ColourSystem:
             # Normalize the rgb vector
             rgb /= np.max(rgb)
 
-        if out_fmt == 'html':
-            return self.rgb_to_hex(rgb)
-        return rgb
+        print(rgb)
+        return self.rgb_to_hex(rgb)
 
     def rgb_to_hex(self, rgb):
         """Convert from fractional rgb values to HTML-style hex string."""
@@ -103,11 +105,11 @@ class ColourSystem:
             return XYZ
         return XYZ / den
 
-    def spec_to_rgb(self, spec, out_fmt=None):
+    def spectral_radiance_to_rgb(self, spec):
         """Convert a spectrum to an rgb value."""
 
         xyz = self.spec_to_xyz(spec)
-        return self.xyz_to_rgb(xyz, out_fmt)
+        return self.xyz_to_rgb(xyz)
 
 
 def planck(lam, T):
@@ -124,12 +126,49 @@ def planck(lam, T):
     return B
 
 
-lam = np.arange(380., 781., 5)
-spec = planck(lam, 7000)
+def custom_sd_to_srgb(t: float) -> str:
+    blackbody_spectrum = colour.sd_blackbody(t, colour.SpectralShape(380, 780, 5)) * 1e9
+    xyz = colour.sd_to_XYZ(
+        colour.SpectralDistribution(blackbody_spectrum),
+        cmfs=colour.MSDS_CMFS["CIE 2015 10 Degree Standard Observer"],
+        illuminant=colour.SDS_ILLUMINANTS["D65"])
+    xyz /= np.sum(xyz)
+    srgb = colour.XYZ_to_sRGB(xyz)
+    # srgb -= min if (min := np.min(srgb)) < 0 else 0
+    srgb /= np.max(srgb)
+    srgb = np.clip(srgb, 0, 1)
+    srgb_hex = colour.notation.RGB_to_HEX(srgb)
+    print(xyz, srgb, srgb_hex)
+    return srgb_hex, tuple(srgb)
+
+
+def stolen_sd_to_rgb(t: float) -> str:
+    blackbody_spectrum = colour.sd_blackbody(t, colour.SpectralShape(380, 780, 5)) * 1e9
+    srgb = colour_system.spectral_radiance_to_rgb(blackbody_spectrum.values)
+    xyz = colour_system.spec_to_xyz(blackbody_spectrum.values)
+    print(xyz, srgb)
+    return srgb
+
+
+def plot_gradients(a_colours: list[tuple[float, float, float]], b_colours: list[tuple[float, float, float]]) -> None:
+    figure, (ax1, ax2) = matplotlib.pyplot.subplots(2, 1, sharex=True)
+    for i in range(min(len(a_colours), len(b_colours))):
+        ax1.bar(i, 1, color=a_colours[i], edgecolor="none")
+        ax2.bar(i, 1, color=b_colours[i], edgecolor="none")
+    plt.tight_layout()
+    plt.show()
+
+
+T = 1500
+blackbody_spectrum = colour.sd_blackbody(T, colour.SpectralShape(380, 780, 5)) * 1e9
+
 colour_system = ColourSystem(
     red=xyz_from_xy(0.67, 0.33),
     green=xyz_from_xy(0.21, 0.71),
     blue=xyz_from_xy(0.15, 0.06),
     white=xyz_from_xy(0.3127, 0.3290))
-srgb = colour_system.spec_to_rgb(spec, out_fmt='html')
-print(srgb)
+
+temp_range = range(100, 10000, 250)
+plot_gradients(
+    [custom_sd_to_srgb(T)[1] for T in temp_range],
+    [tuple(int(stolen_sd_to_rgb(T).lstrip("#")[i:i + 2], 16) / 255 for i in (0, 2, 4)) for T in temp_range])
