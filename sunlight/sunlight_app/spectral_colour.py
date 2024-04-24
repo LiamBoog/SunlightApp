43eb0477627dc358713f417058430dce2eec0689
Liam Boog <mothercow5@gmail.com>
2024-04-23 21:06:54 -0400

    solar zenith angle

--- sunlight/sunlight_app/spectral_colour.py
+++ sunlight/sunlight_app/spectral_colour.py
@@ -1,10 +1,14 @@
 import colour
+import ephem
+import datetime
+import geocoder
+import python_weather
 import numpy as np
 from numpy.typing import NDArray
 from scipy.constants import h, c, k
 
 
-SOLAR_BLACKBODY_TEMPERATURE = 5800
+SOLAR_BLACKBODY_TEMPERATURE = 5777
 
 
 def blackbody_temperature_to_srgb(t: float) -> str:
@@ -21,17 +25,17 @@ def blackbody_temperature_to_srgb(t: float) -> str:
     return str(srgb_hex)
 
 
-def get_blackbody(wavelength: NDArray[int]) -> colour.SpectralDistribution:
+def solar_spectral_radiance(wavelength: NDArray[int], zenith_angle: float) -> colour.SpectralDistribution:
     wavelength_meters = wavelength * 1e-9
     exponent = h * c / (k * wavelength_meters * SOLAR_BLACKBODY_TEMPERATURE)
     denominator = wavelength_meters ** 5 * (np.exp(exponent) - 1)
     spectral_radiance = (2 * h * c * c) / denominator
-    scattering_coefficient = get_total_scattering_coefficient(0, wavelength_meters)
+    scattering_coefficient = get_total_scattering_coefficient(zenith_angle, wavelength_meters)
     spectral_radiance = scattering_coefficient * spectral_radiance
     return colour.SpectralDistribution(spectral_radiance, wavelength)
 
 
-def get_air_mass(zenith_angle: float) -> float:
+def air_mass_coefficient(zenith_angle: float) -> float:
     r = 6371 / 9
     a = 2 * r + 1
     cosine = np.cos(np.radians(zenith_angle))
@@ -42,13 +46,13 @@ def get_air_mass(zenith_angle: float) -> float:
 
 def get_total_scattering_coefficient(zenith_angle: float, wavelength: NDArray[float]) -> float:
     scattering_coefficient = 1e-26
-    x = -(get_air_mass(zenith_angle) * scattering_coefficient / wavelength ** 4)
+    x = -(air_mass_coefficient(zenith_angle) * scattering_coefficient / wavelength ** 4)
     co = np.exp(x)
     return co
 
 
-def spectral_radiance_to_srgb() -> str:
-    blackbody_spectrum = get_blackbody(np.array([i for i in range(380, 781, 5)]))
+def solar_zenith_to_srgb(zenith_angle: float) -> str:
+    blackbody_spectrum = solar_spectral_radiance(np.array([i for i in range(380, 781, 5)]), zenith_angle)
     xyz = colour.sd_to_XYZ(
         colour.SpectralDistribution(blackbody_spectrum),
         cmfs=colour.MSDS_CMFS["CIE 2015 10 Degree Standard Observer"],
@@ -61,4 +65,17 @@ def spectral_radiance_to_srgb() -> str:
     return str(srgb_hex)
 
 
-print(spectral_radiance_to_srgb())
+def solar_zenith_angle() -> float:
+    sun: ephem.Body = ephem.Sun()
+    latitude, longitude = geocoder.ip("me").latlng
+    observer = ephem.Observer()
+    observer.lat = latitude
+    observer.lon = longitude
+    observer.date = ephem.Date(datetime.datetime.now(datetime.UTC))
+    observer.temperature = 15
+    sun.compute(observer)
+    return 90 + np.degrees(sun.alt)
+
+
+print((zenith := solar_zenith_angle()))
+print(solar_zenith_to_srgb(zenith))
