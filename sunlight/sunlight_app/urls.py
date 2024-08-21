d4e513a43027062ce65bcfd9b6518bd55831fa71
Liam Boog <mothercow5@gmail.com>
2024-08-21 17:17:04 -0400

    remove heinous hardcoded url names

--- sunlight/sunlight_app/urls.py
+++ sunlight/sunlight_app/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path, register_converter
 from . import views, converters
 
+
+GET_RANDOM_COLOUR = views.get_random_colour.__name__
+GET_SUNLIGHT_COLOUR = views.get_sunlight_colour.__name__
+COLOURED_INDEX = views.coloured_index.__name__
+RENDER_BLACKBODY = views.render_blackbody.__name__
+
 register_converter(converters.HexadecimalConverter, "hex")
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("get_colour", views.get_sunlight_colour, name="get_sunlight_colour"),
-    path("<hex:colour>", views.coloured_index, name="coloured_index"),
-    path("<int:temperature>", views.render_blackbody, name="blackbody"),
+    path("get_colour", views.get_random_colour, name=GET_RANDOM_COLOUR),
+    path("get_sunlight_colour", views.get_sunlight_colour, name=GET_SUNLIGHT_COLOUR),
+    path("<hex:colour>", views.coloured_index, name=COLOURED_INDEX),
+    path("<int:temperature>", views.render_blackbody, name=RENDER_BLACKBODY),
 ]
--- sunlight/sunlight_app/views.py
+++ sunlight/sunlight_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, HttpRequest
 from .models import Program
 from django.shortcuts import render, redirect
 import random
-from . import spectral_colour
+from . import spectral_colour, urls
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -12,19 +12,27 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request, "sunlight/index.html", context)
 
 
-def get_sunlight_colour(request: HttpRequest):
+def get_random_colour(request: HttpRequest):
     colour = "".join((hex(random.randint(0, 15))[2:] for _ in range(6)))
-    return redirect("coloured_index", colour=colour)
+    return redirect(urls.COLOURED_INDEX, colour=colour)
+
+
+def get_sunlight_colour(request: HttpRequest):
+    zenith = spectral_colour.solar_zenith_angle()
+    colour = spectral_colour.solar_zenith_to_srgb(zenith)
+    return redirect(urls.COLOURED_INDEX, colour=colour[1:])
 
 
 def coloured_index(request: HttpRequest, colour: str):
     context = {
         "programs": [program.name for program in Program.objects.all()],
-        "colour": f"#{colour}"
+        "colour": f"#{colour}",
+        "button_url1": urls.GET_RANDOM_COLOUR,
+        "button_url2": urls.GET_SUNLIGHT_COLOUR,
     }
     return render(request, "sunlight/index.html", context)
 
 
 def render_blackbody(request: HttpRequest, temperature: int):
     colour = spectral_colour.blackbody_temperature_to_srgb(float(temperature))[1:]
-    return redirect("coloured_index", colour=colour)
+    return redirect(urls.COLOURED_INDEX, colour=colour)
